9e39c3dcbc06e273c2d73308b757c86d4ed8c052
salty-walty <waltonsj3@gmail.com>
2019-09-16 12:01:29 -0400

    sucessfully deployed

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+	'YOUR METAMASK SEED PHRASE',
+	'YOUR RINKEBY ENDPOINT URL'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('Attempting to deploy from account', accounts[0])
+
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+		.send({ from: accounts[0] });
+
+	console.log('Contract deployed to ', result.options.address)
+};
+
+deploy();
+
